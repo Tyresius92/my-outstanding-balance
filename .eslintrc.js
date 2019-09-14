e2f48dfcacedcd5d1459bfefd88e593973e5be78
Tyrel <tyrelclayton92@gmail.com>
2019-09-14 07:31:16 -0400

    finish eslint config and add sonarqube

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
         es6: true,
         jest: true
     },
-    extends: "eslint:recommended",
+    extends: ["eslint:recommended", "plugin:react/recommended"],
     globals: {
         Atomics: "readonly",
         SharedArrayBuffer: "readonly"
     },
+    parser: "babel-eslint",
     parserOptions: {
         ecmaFeatures: {
             jsx: true
@@ -21,6 +22,35 @@ module.exports = {
         indent: ["error", 2],
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
-        semi: ["error", "always"]
+        semi: ["error", "always"],
+        "no-console": "error",
+        "array-callback-return": "error",
+        complexity: ["error", 15],
+        "consistent-return": "error",
+        curly: "error",
+        "default-case": "error",
+        "default-param-last": "error",
+        "dot-location": ["error", "property"],
+        "dot-notation": "error",
+        eqeqeq: "error",
+        "no-alert": "error",
+        "no-else-return": ["error", { allowElseIf: false }],
+        "no-empty-function": "error",
+        "no-eval": "error",
+        "no-floating-decimal": "error",
+        "no-lone-blocks": "error",
+        "no-var": "error",
+        "no-magic-numbers": "error",
+        "no-param-reassign": "error",
+        "no-return-assign": "error",
+        "no-unmodified-loop-condition": "error",
+        "no-unused-expressions": "error",
+        yoda: "error",
+        "no-undefined": "error"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
     }
 };
