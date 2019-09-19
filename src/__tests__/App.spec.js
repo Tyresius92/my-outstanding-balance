12e166bbc95cc0a86f125c896192ceeb1de2e82e
Tyrel <tyrelclayton92@gmail.com>
2019-09-19 17:41:25 -0400

    put all config in package.json

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -6,8 +6,9 @@ import Navbar from "../components/Navbar";
 describe("App", () => {
   it("has a Navbar component", () => {
     const wrapper = shallow(<App />);
+    const expecedLength = 1;
 
-    expect(wrapper.find(Navbar).length).toBe(1);
+    expect(wrapper.find(Navbar).length).toBe(expecedLength);
 
     wrapper.unmount();
   });
--- src/components/__tests__/Navbar.spec.js
+++ src/components/__tests__/Navbar.spec.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import Navbar from "../Navbar";
 
 describe("Navbar", () => {
   it("has a brand element", () => {
     const wrapper = mount(<Navbar />);
+    const expectedLength = 1;
 
     const brands = wrapper.find({ enzymeid: "navbarBrand" }).hostNodes();
 
-    expect(brands.length).toBe(1);
+    expect(brands.length).toBe(expectedLength);
     wrapper.unmount();
   });
 
@@ -23,14 +24,15 @@ describe("Navbar", () => {
 
   it("has a toggle and collapse pair", () => {
     const wrapper = mount(<Navbar />);
+    const expectedLength = 1;
 
     const toggle = wrapper.find({ enzymeid: "navbarToggle" }).hostNodes();
     const collapseSection = wrapper
       .find({ enzymeid: "navbarCollapse" })
       .hostNodes();
 
-    expect(toggle.length).toBe(1);
-    expect(collapseSection.length).toBe(1);
+    expect(toggle.length).toBe(expectedLength);
+    expect(collapseSection.length).toBe(expectedLength);
     wrapper.unmount();
   });
 });
