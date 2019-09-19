d35ac1cd7a4c3b79201ac8bb0a4e181faf709455
Tyrel <tyrelclayton92@gmail.com>
2019-09-19 17:51:36 -0400

    fix code smell

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import BSNavbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
-const MobNavbar = () => (
-  <Navbar bg="mob" expand="lg" variant="mob">
+const Navbar = () => (
+  <BSNavbar bg="mob" expand="lg" variant="mob">
     <Container>
-      <Navbar.Brand enzymeid="navbarBrand">My Outstanding Balance</Navbar.Brand>
-      <Navbar.Toggle enzymeid="navbarToggle" aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse enzymeid="navbarCollapse" id="basic-navbar-nav">
+      <BSNavbar.Brand enzymeid="navbarBrand">
+        My Outstanding Balance
+      </BSNavbar.Brand>
+      <BSNavbar.Toggle
+        enzymeid="navbarToggle"
+        aria-controls="basic-navbar-nav"
+      />
+      <BSNavbar.Collapse enzymeid="navbarCollapse" id="basic-navbar-nav">
         <div />
-      </Navbar.Collapse>
+      </BSNavbar.Collapse>
     </Container>
-  </Navbar>
+  </BSNavbar>
 );
 
-export default MobNavbar;
+export default Navbar;
